f7ad02a0fcb08454a9e438ef091302282fe0ff0c
ahmedooo1 <aahmadooo997@gmail.com>
2022-01-04 15:39:34 +0100

    f..h...accueil.css..les imgs

---
+++ footer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+</head>
+
+<body>
+
+    <footer>
+
+
+
+        <div class="contact">
+            <h3>Contact :</h3>
+            <ul>
+
+                <li><img src="SM_icons/facebook.png" alt="fb"><br> Facebook</li>
+                <li><img src="SM_icons/instagram.png" alt="in"><br> Instagram</li>
+                <li><img src="SM_icons/linkedin.png" alt="lnk"><br> Linkedin</li>
+                <li><img src="SM_icons/twitter.png" alt="tr"><br> Twitter</li>
+
+
+            </ul>
+        </div>
+
+        <div class="More :">
+            
+            <h3><a href="#"> Mention Légales . . .</a></h3>
+
+
+        </div>
+    </footer>
+
+</body>
+
+</html>
