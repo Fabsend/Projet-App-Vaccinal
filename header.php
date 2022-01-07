ee0481ced7ab178eabd5f358e0d9c5b65bc3502b
ahmedooo1 <aahmadooo997@gmail.com>
2022-01-07 11:40:28 +0100

    header btn deconnexion

--- header.php
+++ header.php
@@ -6,12 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+
     <title>Document</title>
 </head>
 
 <body>
     <header>
+
         <a href="carnet.php"><img src="vaccina logo.png" alt="logo"></a>
+
+        <button>Déconnexion</button>
 
     </header>
 </body>
