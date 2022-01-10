8edb524a60b04274ec7e291bbe4f83d16a39c43f
Fabsend <97021007+Fabsend@users.noreply.github.com>
2022-01-10 09:53:14 +0100

    push

--- carnet.php
+++ carnet.php
@@ -1,11 +1,4 @@
-<?php 
-
-function debug($tableau)
-{
-    echo '<pre style="height:200px;overflow: scroll; font-size: .8em;padding: 10px;font-family: Consolas, Monospace; background-color: #000;color:#fff;">';
-    print_r($tableau);
-    echo '</pre>';
-}
+<?php
 
 $pdo = new PDO('mysql:host=localhost;dbname=mon_carnet', "root", "root");
 $requestvaccin = $pdo->prepare("SELECT * FROM `vaccin` JOIN utilisateur ON vaccin.utilisateur_id=utilisateur.id"); //Préparer
@@ -41,7 +34,7 @@ $id = $_SESSION['id'];
 $reqprofil = $pdo->prepare("SELECT * FROM utilisateur WHERE `id` = '$id'"); //Préparer
 $reqprofil->execute(); //Executer 
 $affprofil = $reqprofil->fetchAll();
-debug($affprofil);
+
 
 ?>
 
